054ec9e1432f72ef9d1a9b56613aae6e05463475
shanye <shanye.xzq@alibaba-inc.com>
2025-03-20 15:22:06 +0800

    module: improve data source count

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,3 +1,144 @@
 output "services" {
-  value = module.enable.services
+  description = "The Collection that you specified."
+  value       = module.enable.services
+}
+
+output "event_bridge_service" {
+  description = "The enable status of event bridge service"
+  value       = module.enable.event_bridge_service
+}
+
+output "fnf_service" {
+  description = "The enable status of fnf service"
+  value       = module.enable.fnf_service
+}
+
+output "pvtz_service" {
+  description = "The enable status of pvtz service"
+  value       = module.enable.pvtz_service
+}
+
+output "api_gateway_service" {
+  description = "The enable status of api gateway service"
+  value       = module.enable.api_gateway_service
+}
+
+output "cdn_service" {
+  description = "The enable status of cdn service"
+  value       = module.enable.cdn_service
+}
+
+output "cen_transit_router_service" {
+  description = "The enable status of cen transit router service"
+  value       = module.enable.cen_transit_router_service
+}
+
+output "cloud_storage_gateway_service" {
+  description = "The enable status of cloud storage gateway service"
+  value       = module.enable.cloud_storage_gateway_service
+}
+
+output "ack_service" {
+  description = "The enable status of ack service"
+  value       = module.enable.ack_service
+}
+
+output "dcdn_service" {
+  description = "The enable status of dcdn service"
+  value       = module.enable.dcdn_service
+}
+
+output "data_works_service" {
+  description = "The enable status of data works service"
+  value       = module.enable.data_works_service
+}
+
+output "fc_service" {
+  description = "The enable status of fc service"
+  value       = module.enable.fc_service
+}
+
+output "iot_service" {
+  description = "The enable status of iot service"
+  value       = module.enable.iot_service
+}
+
+output "kms_service" {
+  description = "The enable status of kms service"
+  value       = module.enable.kms_service
+}
+
+output "log_service" {
+  description = "The enable status of log service"
+  value       = module.enable.log_service
+}
+
+output "nas_service" {
+  description = "The enable status of nas service"
+  value       = module.enable.nas_service
+}
+
+output "oss_service" {
+  description = "The enable status of oss service"
+  value       = module.enable.oss_service
+}
+
+output "privatelink_service" {
+  description = "The enable status of privatelink service"
+  value       = module.enable.privatelink_service
+}
+
+output "ons_service" {
+  description = "The enable status of ons service"
+  value       = module.enable.ons_service
+}
+
+output "ots_service" {
+  description = "The enable status of ots service"
+  value       = module.enable.ots_service
+}
+
+output "cr_service" {
+  description = "The enable status of cr service"
+  value       = module.enable.cr_service
+}
+
+output "sae_service" {
+  description = "The enable status of sae service"
+  value       = module.enable.sae_service
+}
+
+output "datahub_service" {
+  description = "The enable status of datahub service"
+  value       = module.enable.datahub_service
+}
+
+output "edas_service" {
+  description = "The enable status of edas service"
+  value       = module.enable.edas_service
+}
+
+output "cloud_sso_service" {
+  description = "The enable status of cloud sso service"
+  value       = module.enable.cloud_sso_service
+}
+
+output "hbr_service" {
+  description = "The enable status of hbr service"
+  value       = module.enable.hbr_service
+}
+
+output "vpc_flow_log_service" {
+  description = "The enable status of vpc flow log service"
+  value       = module.enable.vpc_flow_log_service
+}
+
+output "vpc_traffic_mirror_service" {
+  description = "The enable status of vpc traffic mirror service"
+  value       = module.enable.vpc_traffic_mirror_service
+}
+
+output "vs_service" {
+  description = "The enable status of vs service"
+  value       = module.enable.vs_service
 }
--- main.tf
+++ main.tf
@@ -1,107 +1,135 @@
 data "alicloud_event_bridge_service" "open" {
+  count  = contains(keys(var.services), "alicloud_event_bridge_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_event_bridge_service", "Off")
 }
 
 data "alicloud_fnf_service" "open" {
+  count  = contains(keys(var.services), "alicloud_fnf_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_fnf_service", "Off")
 }
 
 data "alicloud_pvtz_service" "open" {
+  count  = contains(keys(var.services), "alicloud_pvtz_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_pvtz_service", "Off")
 }
 
 data "alicloud_api_gateway_service" "open" {
+  count  = contains(keys(var.services), "alicloud_api_gateway_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_api_gateway_service", "Off")
 }
 
 data "alicloud_cdn_service" "open" {
+  count                = contains(keys(var.services), "alicloud_cdn_service") ? 1 : 0
   enable               = lookup(var.services, "alicloud_cdn_service", "Off")
   internet_charge_type = var.cdn_charge_type
 }
 
 data "alicloud_cen_transit_router_service" "open" {
+  count  = contains(keys(var.services), "alicloud_cen_transit_router_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_cen_transit_router_service", "Off")
 }
 
 data "alicloud_cloud_storage_gateway_service" "open" {
+  count  = contains(keys(var.services), "alicloud_cloud_storage_gateway_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_cloud_storage_gateway_service", "Off")
 }
 
 data "alicloud_ack_service" "open" {
+  count  = contains(keys(var.services), "alicloud_ack_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_ack_service", "Off")
   type   = var.ack_charge_type
 }
 
 data "alicloud_dcdn_service" "open" {
+  count  = contains(keys(var.services), "alicloud_dcdn_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_dcdn_service", "Off")
 }
 data "alicloud_data_works_service" "open" {
+  count  = contains(keys(var.services), "alicloud_data_works_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_data_works_service", "Off")
 }
 
 data "alicloud_fc_service" "open" {
+  count  = contains(keys(var.services), "alicloud_fc_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_fc_service", "Off")
 }
 data "alicloud_iot_service" "open" {
+  count  = contains(keys(var.services), "alicloud_iot_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_iot_service", "Off")
 }
 data "alicloud_kms_service" "open" {
+  count  = contains(keys(var.services), "alicloud_kms_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_kms_service", "Off")
 }
 data "alicloud_log_service" "open" {
+  count  = contains(keys(var.services), "alicloud_log_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_log_service", "Off")
 }
 data "alicloud_nas_service" "open" {
+  count  = contains(keys(var.services), "alicloud_nas_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_nas_service", "Off")
 }
 
 data "alicloud_oss_service" "open" {
+  count  = contains(keys(var.services), "alicloud_oss_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_oss_service", "Off")
 }
 data "alicloud_privatelink_service" "open" {
+  count  = contains(keys(var.services), "alicloud_privatelink_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_privatelink_service", "Off")
 }
 data "alicloud_ons_service" "open" {
+  count  = contains(keys(var.services), "alicloud_ons_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_ons_service", "Off")
 }
 
 data "alicloud_ots_service" "open" {
+  count  = contains(keys(var.services), "alicloud_ots_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_ots_service", "Off")
 }
 
 data "alicloud_cr_service" "open" {
+  count    = contains(keys(var.services), "alicloud_cr_service") ? 1 : 0
   enable   = lookup(var.services, "alicloud_cr_service", "Off")
   password = var.cr_pwd
 }
 
 data "alicloud_sae_service" "open" {
+  count  = contains(keys(var.services), "alicloud_sae_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_sae_service", "Off")
 }
 
 data "alicloud_datahub_service" "open" {
+  count  = contains(keys(var.services), "alicloud_datahub_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_datahub_service", "Off")
 }
 
 data "alicloud_edas_service" "open" {
+  count  = contains(keys(var.services), "alicloud_edas_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_edas_service", "Off")
 }
 
 data "alicloud_cloud_sso_service" "open" {
+  count  = contains(keys(var.services), "alicloud_cloud_sso_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_cloud_sso_service", "Off")
 }
 
 data "alicloud_hbr_service" "open" {
+  count  = contains(keys(var.services), "alicloud_hbr_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_hbr_service", "Off")
 }
 
 data "alicloud_vpc_flow_log_service" "open" {
+  count  = contains(keys(var.services), "alicloud_vpc_flow_log_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_vpc_flow_log_service", "Off")
 }
 
 data "alicloud_vpc_traffic_mirror_service" "open" {
+  count  = contains(keys(var.services), "alicloud_vpc_traffic_mirror_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_vpc_traffic_mirror_service", "Off")
 }
 
 data "alicloud_vs_service" "open" {
+  count  = contains(keys(var.services), "alicloud_vs_service") ? 1 : 0
   enable = lookup(var.services, "alicloud_vs_service", "Off")
 }
